f69fa55ce8a96527247f579b8fb2a34ee8860d5c
jhonatan1043 <jhonat.rodri@gmail.com>
2020-02-05 16:51:10 -0500

    commit

---
+++ testcondorlabbackend/database/db.sql
@@ -0,0 +1,27 @@
+/*CREATE DATABASE testdb;
+
+USE testdb;
+
+CREATE TABLE tasks (
+   idTask int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   title varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+   task varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+   idUser int(11) NOT NULL,
+   statusTask int(11) NOT NULL,
+   createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updatedAt datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+CREATE TABLE users (
+  idUser int(11) NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+  userName varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  email varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+
+INSERT INTO testbd.users
+(userName,email) VALUES ('jhonatan','jhonat.rodri@yahoo.com'),('fulano','fulanodetal@yahoo.com');*/
+
+
